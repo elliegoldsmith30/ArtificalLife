506e67c9d848592cbf7dc879d68f6149d1140142
Ellie Goldsmith <92822567+elliegoldsmith30@users.noreply.github.com>
2023-01-09 15:14:53 -0600

    towers

--- generate.py
+++ generate.py
@@ -1,4 +1,19 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x=1
+y=1
+z=1
+for x in range(5):
+	for y in range(5):
+		length=1
+		width=1
+		height=1
+		for i in range(10):
+			pyrosim.Send_Cube(name = "Box" + str(i), pos = [x,y, i + 0.5], size = [length,width,height])
+			length = 0.9*length
+			width = 0.9*width
+			height = 0.9*height
 pyrosim.End()
