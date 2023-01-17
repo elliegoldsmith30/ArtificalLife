b87cd1f1561fd1ec0b5085c2050375c17184569a
Ellie Goldsmith <92822567+elliegoldsmith30@users.noreply.github.com>
2023-01-17 12:26:47 -0600

    adding more sensors

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -9,10 +10,11 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-for x in range(1000):
+backLegSensorValues = numpy.zeros(10000)
+for x in range(10000):
 	p.stepSimulation()
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-	print(backLegTouch)
+	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/1000)
+print(backLegSensorValues)
 p.disconnect()
 
